9b7d6c1143a05c17ea75b108ee499a4c9c4d7359
«Aleksey» <alshopik@yandex.ru>
2022-12-02 00:53:58 +0300

    task005_weekday

---
+++ HomeWork/Task005/Program.cs
@@ -0,0 +1,32 @@
+﻿// По заданному номеру дня недели вывести его название
+
+int number = 6;
+
+if (number == 1)
+{
+    Console.WriteLine("ПОНЕДЕЛЬНИК");
+}
+if (number == 2)
+{
+    Console.WriteLine("ВТОРНИК");
+}
+if (number == 3)
+{
+    Console.WriteLine("СРЕДА");
+}
+if (number == 4)
+{
+    Console.WriteLine("ЧЕТВЕРГ");
+}
+if (number == 5)
+{
+    Console.WriteLine("ПЯТНИЦА");
+}
+if (number == 6)
+{
+    Console.WriteLine("СУББОТА");
+}
+if (number == 7)
+{
+    Console.WriteLine("ВОСКРЕСЕНЬЕ");
+}
