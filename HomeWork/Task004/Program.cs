2bfeb277a5697a878baa802c81fdc2f384d55f0a
«Aleksey» <alshopik@yandex.ru>
2022-12-02 00:35:34 +0300

    task004_the_first_is_the_square_of_the_second

---
+++ HomeWork/Task004/Program.cs
@@ -0,0 +1,19 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+int a = 121;
+int b = 10;
+
+if (a == b * b)
+{
+    Console.Write("число ");
+    Console.Write(a);
+    Console.Write(" является квадратом числа ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("число ");
+    Console.Write(a);
+    Console.Write(" не является квадратом числа ");
+    Console.Write(b);
+}
