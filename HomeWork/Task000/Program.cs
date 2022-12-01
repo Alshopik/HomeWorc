923c545aba02c63bc1e73f6ea66457049338f3b4
«Aleksey» <alshopik@yandex.ru>
2022-12-01 21:32:27 +0300

    task001_commit2

--- HomeWork/Task000/Program.cs
+++ HomeWork/Task000/Program.cs
@@ -4,3 +4,37 @@
 //System.Console.WriteLine(random);
 
 //Напишите программу, которая на вход принимает два числа и выдает, какое чило больше, а какое меньше.
+//a = 5, b = 7 -> max = 7
+//a = 2, b = 10 -> max = 10
+//a = -9, b = -3 -> max = -3
+
+// start
+
+//int a = 1;
+//int b = -9;
+
+//int max = 0;
+
+//if (a > b)
+//{
+//    max = a;
+//}
+//else
+//{
+//    max = b;
+//}
+
+//Console.WriteLine(max);
+
+// end
+
+int a = 100;
+int b = -9;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+Console.WriteLine(max);
