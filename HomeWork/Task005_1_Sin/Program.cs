051866db98047dd24b7119e7be6330a894ce51ef
aleksey <alshopik@yandex.ru>
2022-12-06 18:32:02 +0300

    Task005_1_Sin

---
+++ HomeWork/Task005_1_Sin/Program.cs
@@ -0,0 +1,5 @@
+﻿//5. Написать программу вычисления значения функции y = sin(a)
+
+int a = 90;       //в градусах        
+double result = Math.Sin(a*Math.PI/180);     //число Пи - Math.PI; перевод градусов в радианы  a*Math.PI/180
+Console.WriteLine(result);
