55f0965cb897987641f992e8516ecdf284aed52a
aleksey <alshopik@yandex.ru>
2022-12-07 17:24:21 +0300

    HomeWork3

---
+++ HomeWork/Task019_Palidrom/Program.cs
@@ -0,0 +1,31 @@
+﻿//19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Clear();
+
+int number = new Random().Next(10000, 99999);           //случайное пятизначное число  
+//int number = 12821;                                  //ручная проверка
+
+int first = number / 10000 % 10;
+int second = number / 1000 % 10;
+int fourth = number / 10 % 10;
+int fifht = number % 10;                       // % 10 - остаток от деления на 10 - это последняя цифра числа
+
+if (first == first && second == fourth)       // палидром  — число, буквосочетание, слово или текст, одинаково читающееся в обоих направлениях
+{
+    Console.WriteLine($"Число {number} является палидромом");   // ДА - является палидромом
+}
+else
+{
+Console.WriteLine($"Число {number} не является палидромом");   // НЕТ - не является палидромом
+}
+
+// Console.WriteLine(number);
+
+// Console.WriteLine(first);
+// Console.WriteLine(second);
+// Console.WriteLine(fourth);
+// Console.WriteLine(fifht);
---
+++ HomeWork/Task023_CubeTable/Program.cs
@@ -0,0 +1,19 @@
+﻿// 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Input(string text)
+  {
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+  }
+
+Console.Clear();
+
+int number = Input("Введите число и нажмите Enter: ");
+
+for (int i = 1; i <= number; i++)
+{
+Console.Write($"{Math.Pow(i, 3)} ");
+}
