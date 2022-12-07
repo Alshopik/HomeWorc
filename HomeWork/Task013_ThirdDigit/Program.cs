ae12ec6a6e2a20ab08ed896e7d7f3e444bdab4ac
aleksey <alshopik@yandex.ru>
2022-12-07 14:48:58 +0300

    HomeWork2

--- HomeWork/Task013_ThirdDigit/Program.cs
+++ HomeWork/Task013_ThirdDigit/Program.cs
@@ -1,7 +1,25 @@
-﻿
+﻿// 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-int a = 245;
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-int b = (int)Math.Floor(Math.Log10(a)) - 2;
-int d = (int)Math.Pow(10, b);
-Console.WriteLine(a / d % 10);
+int Input(string text)
+  {
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+  }
+  
+  Console.Clear();
+  int a = Input("Введите число и нажмите Enter: ");
+
+if (a > 99)
+{
+    int b = (int)Math.Floor(Math.Log10(a)) - 2;
+    int d = (int)Math.Pow(10, b);
+    Console.WriteLine("Третья цифра числа " + a / d % 10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
