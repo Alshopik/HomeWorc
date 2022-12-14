06900861e7f309ad8f239cfb213bf58288ecc0ac
aleksey <alshopik@yandex.ru>
2022-12-14 18:55:19 +0300

    Seminar04-1

---
+++ HomeWork/Seminar04/Task01_Sum1toA/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+
+// 1.Метод2 ввода числа
+int ReadInt(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+// 2. Метод суммирования чисел от 1 до А
+int Sum(int A)
+{
+    int Sum = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        Sum += i;
+    }
+    return Sum;
+}
+
+Console.Clear();
+int A = ReadInt("Введите число и нажмите Enter: ");
+Console.WriteLine($"{Sum(A)}");
