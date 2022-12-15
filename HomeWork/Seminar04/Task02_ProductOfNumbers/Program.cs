b4e86fc64457a33498f6288bdd22b4741052a49d
aleksey <alshopik@yandex.ru>
2022-12-15 13:52:18 +0300

    Seminar04-2

---
+++ HomeWork/Seminar04/Task02_ProductOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+
+// 1.Метод2 ввода числа
+int ReadInt(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+// 2. Метод суммирования чисел от 1 до N
+int Pro(int N)
+{
+    int Pro = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        Pro *= i;
+    }
+    return Pro;
+}
+
+Console.Clear();
+int N = ReadInt("Введите число и нажмите Enter: ");
+Console.WriteLine($"Произведение чисел от 1 до {N}: {Pro(N)}");
